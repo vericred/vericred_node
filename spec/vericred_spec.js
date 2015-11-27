cc76d8ac04770067531ae35f3410b47018c0bd37
Dan Langevin <dan.langevin@gmail.com>
2015-11-27 09:52:26 -0500

    Resource definitions

--- spec/vericred_spec.js
+++ spec/vericred_spec.js
@@ -6,4 +6,15 @@ describe('Vericred', function () {
     Vericred.config.apiKey = 'foo';
     expect(Vericred.config.apiKey).to.eql('foo');
   });
+
+  describe('resources', function () {
+    it('defines the resources', function () {
+      expect(typeof(Vericred.County)).to.eql('function');
+      expect(typeof(Vericred.Plan)).to.eql('function');
+      expect(typeof(Vericred.Provider)).to.eql('function');
+      expect(typeof(Vericred.State)).to.eql('function');
+      expect(typeof(Vericred.ZipCode)).to.eql('function');
+      expect(typeof(Vericred.ZipCounty)).to.eql('function');
+    });
+  });
 });
